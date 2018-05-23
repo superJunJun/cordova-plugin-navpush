46f04a6f1070f34df0c0840e125e4fc8627659b0
lijun <lijun@ishanggang.com>
2018-05-23 20:13:28 +0800

    first commit

---
+++ www/NavPush.js
@@ -0,0 +1,14 @@
+var exec = require('cordova/exec');
+
+// exports.navigationPush = function (arg0, success, error) {
+//     exec(success, error, 'NavPush', 'coolMethod', [arg0]);
+// };
+
+var NavPush = function() {
+};
+
+NavPush.navigationPush = function() {
+    console.warn('Showing keyboard not supported in iOS due to platform limitations.')
+};
+
+module.exports = NavPush;
